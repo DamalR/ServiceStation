afb841b67892067f7b19c53db9d3fd3d4b050272
Damal Ranula <ranuladamal@gmail.com>
2024-06-06 08:19:10 +0530

    Set up React Router DOM

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,53 @@ import Sidebar from './components/Sidebar'
 import Content from './components/Content'
 import Profile from './components/Profile'
 import './App.css'
+import Dashboard from './pages/Dashboard'
+import Appoinments from './pages/Appoinments'
+import Payments from './pages/Payments'
+import Reports from './pages/Reports'
+import Services from './pages/Services'
+import Users from './pages/Users'
+
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
 
 const App = () => {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: (
+        <Dashboard />
+      ),
+    },
+    {
+      path: "appoinments",
+      element: <Appoinments />,
+    },
+    {
+      path: "payments",
+      element: <Payments />,
+    },
+    {
+      path: "reports",
+      element: <Reports />,
+    },
+    {
+      path: "services",
+      element: <Services />,
+    },
+    {
+      path: "users",
+      element: <Users />,
+    },
+  ]);
+
   return (
+ 
     <div>
       <div className='nav-bar'>
         <Profile />
@@ -13,12 +57,13 @@ const App = () => {
       <div className='dashboard'>
         <Sidebar />
         <div className="dashboard-content">
-          <Content />
+        <RouterProvider router={router} />
           </div>
         </div>
       </div>
     </div>
   )
+  
 }
 
 export default App
